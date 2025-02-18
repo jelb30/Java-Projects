f690d0c1d4fe8e55477c7d9ac4a280390736b079
Jill Patel <pateljill2288@gmail.com>
2025-02-18 13:45:43 -0700

    Deleting some class files

--- spring-boot-bootcamp-resources-main/08. rest-api/contacts/starter/contacts/src/main/java/com/ltp/contacts/Exception/ContactNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.ltp.contacts.Exception;
-
-public class ContactNotFoundException extends RuntimeException{
-
-    public ContactNotFoundException(String id){
-        super("CONTACT WITH ID "+id+" DOES NOT EXIST !");
-    }
-}
